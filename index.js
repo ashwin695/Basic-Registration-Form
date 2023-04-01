7768393d22a6c065d41a3af40097d7281f82a8c6
ashwin695 <guptaashwin695@gmail.com>
2023-04-01 22:50:48 +0530

    Add changes local storage

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ function handleSubmit(event)
     console.log(event.target.phone.value)
     console.log(event.target.date.value)
     console.log(event.target.time.value)
+
+    localStorage.setItem('name', event.target.username.value)
+    localStorage.setItem('email', event.target.email.value)
+    localStorage.setItem('phone', event.target.phone.value)
+    localStorage.setItem('date', event.target.date.value)
+    localStorage.setItem('time', event.target.time.value)
 }
